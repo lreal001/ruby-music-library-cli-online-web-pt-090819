e4653160952f7866468531fcb19e0804c8bf6a38
Lessllie Real <rlessllie@yahoo.com>
2019-11-16 14:46:09 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -5,3 +5,38 @@ module Concerns
 end
 
 require_all 'lib'
+
+
+
+class Song
+  
+attr_accessor :name
+   @@all = []
+   def initialize(name)
+     @name= name
+     @@all << self
+   end
+
+def self.destroy_all
+  self.all.clear
+end 
+
+def self.all 
+  @@all
+end 
+
+def self.create(name)
+  song= self.new 
+  song.name= name 
+  @@all << song 
+end 
+
+end 
+
+#Song.create("Thundercat - For Love I Come - dance.mp3")
+
+
+
+
+
+
